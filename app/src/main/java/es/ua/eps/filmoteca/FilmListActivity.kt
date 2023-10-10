95c927d2562b8e1a6e50219fe3c9bb16682552e2
ende <1195562121ende@gmail.com>
2023-10-10 23:56:21 +0200

    Práctica 5 terminado

--- app/src/main/java/es/ua/eps/filmoteca/FilmListActivity.kt
+++ app/src/main/java/es/ua/eps/filmoteca/FilmListActivity.kt
@@ -3,21 +3,22 @@ package es.ua.eps.filmoteca
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.AbsListView.MultiChoiceModeListener
 import android.widget.AdapterView
 import android.widget.ListView
-import android.widget.Toast
-import androidx.core.app.NavUtils
 import es.ua.eps.filmoteca.databinding.ActivityFilmListBinding
 
 
+var actionMode: ActionMode? = null
 
 class FilmListActivity : AppCompatActivity() {
     companion object {
-        private val add_film = Menu.FIRST
-        private val about = Menu.FIRST + 1
+        private const val add_film = Menu.FIRST
+        private const val about = Menu.FIRST + 1
     }
     // Utilizamos el primer id disponible (FIRST)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,15 +28,84 @@ class FilmListActivity : AppCompatActivity() {
 
 
         val listFilm = findViewById<ListView>(R.id.filmList)
+        listFilm.choiceMode = ListView.CHOICE_MODE_MULTIPLE_MODAL
         refresh()
         listFilm.setOnItemClickListener{ parent: AdapterView<*>, view: View,
                                      position: Int, id: Long ->
             //val elemento = adapter.getItem(position)
             goFilm(position)
          }
+
+        listFilm.setSelector(R.drawable.list_selector)
+        listFilm.setMultiChoiceModeListener(
+
+            object : MultiChoiceModeListener{
+                override fun onCreateActionMode(mode: ActionMode,
+                                                menu: Menu): Boolean {
+                    val inflater = mode.menuInflater
+                    inflater.inflate(R.menu.context_menu, menu)
+                    return true
+                }
+
+                override fun onPrepareActionMode(mode: ActionMode,
+                                                 menu: Menu): Boolean {
+                    return false
+                }
+
+                override fun onActionItemClicked(mode: ActionMode,
+                                                 item: MenuItem): Boolean {
+                    return when (item.itemId) {
+                        R.id.menuDeleteSelected -> {
+                            deleteSelectedItems()
+                            mode.finish()
+                            true
+                        }
+                        else -> false
+                    }
+                }
+
+                override fun onDestroyActionMode(mode: ActionMode) {}
+
+                override fun onItemCheckedStateChanged(mode: ActionMode,
+                                                       position: Int, id: Long, checked: Boolean) {
+                    val count = listFilm.checkedItemCount
+                    mode.title = "$count seleccionados"
+
+                    if (checked) {
+                        val selectedView = listFilm.getChildAt(position)
+                        selectedView?.setBackgroundResource(R.drawable.list_selector)
+                    } else {
+                        val selectedView = listFilm.getChildAt(position)
+                        selectedView?.setBackgroundResource(android.R.color.transparent)
+                    }
+                }
+            }
+        )
     }
+
+    private fun deleteSelectedItems() {
+        val selectedItems = ArrayList<Film>() // Lista para almacenar elementos seleccionados
+        val listFilm = findViewById<ListView>(R.id.filmList)
+        for (i in 0 until listFilm.count) {
+            if (listFilm.isItemChecked(i)) {
+                val film = listFilm.getItemAtPosition(i) as Film
+                selectedItems.add(film)
+            }
+        }
+
+        for (item in selectedItems) {
+            // Elimina el elemento de tu lista de datos o de donde estés almacenando los elementos
+            // Por ejemplo, si los elementos están en un ArrayList llamado films:
+            FilmDataSource.films.remove(item)
+        }
+
+        // Notifica al adaptador de la lista que los datos han cambiado
+        (listFilm.adapter as FilmsArrayAdapter).notifyDataSetChanged()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         super.onCreateOptionsMenu(menu)
+
         // Identificador de grupo
         val groupId = Menu.NONE
         // Identificador único del elemento del menú.
@@ -68,7 +138,6 @@ class FilmListActivity : AppCompatActivity() {
             refresh()
             true
         }
-
         return true
     }
 
